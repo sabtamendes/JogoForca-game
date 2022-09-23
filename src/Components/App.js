504d82100bd75b51f3e309bd85992d5ef640e758
Sabta Mendes Alencar <sabtaamendes@gmail.com>
2022-09-22 22:10:59 -0400

    underline renderizado na tela

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,20 +1,85 @@
-import { Body } from "../style";
-import { Words } from "./Words";
+import React from "react";
+import Words from "./Words";
 
+import imagem0 from "../assets/imagem0.png";
+// import imagem1 from "../assets/imagem1.png";
+// import imagem2 from "../assets/imagem2.png";
+// import imagem3 from "../assets/imagem3.png";
+// import imagem4 from "../assets/imagem4.png";
+// import imagem5 from "../assets/imagem5.png";
+// import imagem6 from "../assets/imagem6.png";
 
-import imagem0 from "../Assets/imagem0.png";
 export default function App() {
+    const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    const [habilitarInput, setHabilitarInput] = React.useState("disabled");
+    const [habilitarLetras, setHabilitarLetras] = React.useState([...alfabeto]);
+    const [acionarLetras, setAcionarLetras] = React.useState("disabled");
+    // const [finalizada, setFinalizada] = React.useState([]);
+    const [imagem, setImagem] = React.useState("");
+    const [underline, setUnderline] = React.useState("");
+    const [caractere, setCaractere] = React.useState("");
+    const [palavra, setPalavra] = React.useState([...Words])
+    const [palavrasCorretas, setPalavrasCorretas] = React.useState([]);
+    const [chute, setChute] = React.useState([])
 
-  const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  return (
-    <>
-      <h1>Hangman Game</h1>
-      <span className="palavra-button"><button>Escolher Palavra</button></span>
-        <img src={imagem0} /> 
-      <ul>
-        <li>{alfabeto.map(item => <button><p>{item}</p></button>)}</li>
-      </ul>
-      <div className="palavra"> <span>Já sei a palavra!</span> <input type="text"></input> <button>Chutar</button></div>
-    </>
-  )
+
+    Words.sort(embararalhar)
+    function acionado(index, item) {
+        setHabilitarInput("enabled");
+        setHabilitarLetras(habilitarLetras);
+        setImagem(imagem0);
+        setAcionarLetras("enabled");
+        setCaractere([]);
+
+        //transformando o Array em string
+        for (let i = 0; i < Words.length; i++) {
+            let string = Words[i].toString().split(' ');
+            setUnderline(string.join(' '))
+            for (let i = 0; i < string.length; i++) {
+                let caracter = string[i];
+                console.log(caracter.split('').join(''))
+                setUnderline(caracter.split('').join(''));
+                setCaractere(caracter)
+                setPalavra(caracter);
+            }
+            return string;
+        }
+    }
+
+    function embararalhar() {
+        return Math.random() - 0.5;
+    }
+
+    return (
+        <>
+            <h1>Hangman Game</h1>
+
+            <span className="palavra-button"><button onClick={acionado}>Escolher Palavra</button></span>
+
+            {imagem ? <img src={imagem} alt="texto alternativo" /> : " "}
+
+            {underline.split('').map((letra, i) => {
+                return (
+                    <span className="letrasNaTela" key={i}>{chute.includes(letra) ? letra : ''}</span>)
+            })}
+
+            <ul>
+                <li>
+                    {acionarLetras === "disabled"
+                        ? habilitarLetras.map((item, index) =>
+                            <button type="button" key={index} className="colorDisabled" disabled><p>{item}</p></button>)
+                        : habilitarLetras.map((item, index) =>
+                            <button type="button" key={index} className="colorEnabled" enabled><p>{item}</p></button>)
+                    }
+                </li>
+
+            </ul>
+            <div className="palavra"> <span>Já sei a palavra!</span>
+                {
+                    habilitarInput === "disabled"
+                        ? <input type="text" className="Disabled" disabled></input>
+                        : <input type="text" className="Enabled" enabled></input>
+                } <button>Chutar</button></div>
+        </>
+    )
 }
